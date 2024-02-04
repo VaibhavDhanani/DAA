54271dc66ca28d741134d42ddaadbd887d2103ae
Vaibhav Dhanani <130571437+VaibhavDhanani@users.noreply.github.com>
2024-02-04 11:12:45 +0000

    Done

---
+++ lab7/TopologicalSort.java
@@ -0,0 +1,60 @@
+package lab7;
+
+import java.util.*;
+
+class TopologicalSort {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    TopologicalSort(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void tSort(int v, boolean visited[], Stack<Integer> stack) {
+        visited[v] = true;
+
+        Iterator<Integer> it = adj[v].iterator();
+        while (it.hasNext()) {
+            int i = it.next();
+            if (!visited[i])
+                tSort(i, visited, stack);
+        }
+        stack.push(v);
+    }
+
+    void topologicalSort() {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        boolean visited[] = new boolean[V];
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+
+        for (int i = 0; i < V; i++)
+            if (!visited[i])
+                tSort(i, visited, stack);
+
+        while (!stack.isEmpty())
+            System.out.print(stack.pop() + " ");
+    }
+
+    public static void main(String args[]) {
+        TopologicalSort g = new TopologicalSort(6);
+        g.addEdge(5, 2);
+        g.addEdge(5, 0);
+        g.addEdge(4, 0);
+        g.addEdge(4, 1);
+        g.addEdge(2, 3);
+        g.addEdge(3, 1);
+
+        System.out.println("Topological sorting of the given graph:");
+        g.topologicalSort();
+        System.out.println();
+    }
+}
--- lab8/LongestCommonSub.java
+++ lab8/LongestCommonSub.java
@@ -1,6 +1,5 @@
 package lab8;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class LongestCommonSub {
